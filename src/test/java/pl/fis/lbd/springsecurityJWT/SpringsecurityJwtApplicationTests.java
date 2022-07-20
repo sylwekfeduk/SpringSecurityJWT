f82bac0618353ee45bbf30243b7ce56d80d63d9b
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-20 13:59:40 +0200

    initial commit

---
+++ src/test/java/pl/fis/lbd/springsecurityJWT/SpringsecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fis.lbd.springsecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
